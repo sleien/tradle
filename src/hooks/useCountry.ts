39b181076fe79a409d3f7279fc05452bc31086c2
Noah Schneider <noah.schneider@outlook.com>
2025-01-17 17:51:05 +0100

    fixed stuff locally

--- src/hooks/useCountry.ts
+++ src/hooks/useCountry.ts
@@ -2,18 +2,15 @@ import { csv } from "d3-fetch";
 import { useEffect, useMemo, useState } from "react";
 import { countriesWithImage, Country } from "../domain/countries";
 
-interface DateCountry {
-  country: string;
-  date: string;
-}
-
 export function useCountry(dayString: string): [Country | undefined] {
   const [forcedCountryCode, setForcedCountryCode] = useState("");
 
   useEffect(() => {
-    const randomCountryCode = countriesWithImage[Math.floor(Math.random() * countriesWithImage.length)].code;
+    const randomCountryCode =
+      countriesWithImage[Math.floor(Math.random() * countriesWithImage.length)]
+        .code;
     setForcedCountryCode(randomCountryCode);
-  });
+  }, [dayString]);
 
   const country = useMemo(() => {
     const forcedCountry =
